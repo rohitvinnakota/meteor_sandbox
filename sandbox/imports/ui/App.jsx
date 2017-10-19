9a18cb11449990f43f451e973bc2f07a252a362b
rohitvinnakota <rohitvinnakota@gmail.com>
2017-10-18 23:51:38 -0600

    one

---
+++ sandbox/imports/ui/App.jsx
@@ -0,0 +1,14 @@
+
+
+import React, { Component } from 'react'; 
+// App component - represents the whole app
+export default class App extends Component {
+ 
+  render() {
+    return (
+        <h1>
+          <h1>BRO!!!!!</h1>
+        </h1>
+    );
+  }
+}
